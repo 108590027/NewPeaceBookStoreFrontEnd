3c4e7e804f8a3bba54df16ab9f5c026858ae6bd5
珍奶 <jary90705@gmail.com>
2021-12-26 00:28:26 +0800

    AsideMenu list department

--- src/_metronic/layout/components/aside/AsideMenuMain.tsx
+++ src/_metronic/layout/components/aside/AsideMenuMain.tsx
@@ -1,9 +1,15 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from 'react'
-import {AsideMenuItemWithSub} from './AsideMenuItemWithSub'
 import {AsideMenuItem} from './AsideMenuItem'
+import {CategoryState} from '../../../../app/modules/category/redux/CategoryRedux'
+import {shallowEqual, useSelector} from 'react-redux'
+import {RootState} from '../../../../setup'
 
 export function AsideMenuMain() {
+  const categoryState: CategoryState = useSelector<RootState>(
+    ({category}) => category,
+    shallowEqual
+  ) as CategoryState
   return (
     <>
       <AsideMenuItem
@@ -14,36 +20,23 @@ export function AsideMenuMain() {
       />
       <div className='menu-item'>
         <div className='menu-content pt-8 pb-2'>
-          <span className='menu-section text-muted text-uppercase fs-8 ls-1'>Crafted</span>
+          <span className='menu-section text-muted text-uppercase fs-8 ls-1'>分類</span>
         </div>
       </div>
-      <AsideMenuItemWithSub
-        to='/crafted/pages'
-        title='Pages'
-        fontIcon='bi-archive'
-        icon='/media/icons/duotune/general/gen022.svg'
-      >
-        <AsideMenuItemWithSub to='/crafted/pages/profile' title='Profile' hasBullet={true}>
-          <AsideMenuItem to='/crafted/pages/profile/overview' title='Overview' hasBullet={true} />
-          <AsideMenuItem to='/crafted/pages/profile/projects' title='Projects' hasBullet={true} />
-          <AsideMenuItem to='/crafted/pages/profile/campaigns' title='Campaigns' hasBullet={true} />
-          <AsideMenuItem to='/crafted/pages/profile/documents' title='Documents' hasBullet={true} />
-          <AsideMenuItem
-            to='/crafted/pages/profile/connections'
-            title='Connections'
-            hasBullet={true}
-          />
-        </AsideMenuItemWithSub>
-
-        <AsideMenuItemWithSub to='/crafted/pages/wizards' title='Wizards' hasBullet={true}>
-          <AsideMenuItem
-            to='/crafted/pages/wizards/horizontal'
-            title='Horizontal'
-            hasBullet={true}
-          />
-          <AsideMenuItem to='/crafted/pages/wizards/vertical' title='Vertical' hasBullet={true} />
-        </AsideMenuItemWithSub>
-      </AsideMenuItemWithSub>
+      {categoryState.categories.map((category) => {
+        if (category.is_department) {
+          return (
+            <AsideMenuItem
+              key={`category-${category.id}`}
+              to='/dashboard'
+              icon='/media/icons/duotune/art/art002.svg'
+              title={category.name}
+              fontIcon='bi-app-indicator'
+            />
+          )
+        }
+        return <></>
+      })}
     </>
   )
 }
