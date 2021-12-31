cb68f785d50ffcc248e67cb756d862003b73e3d6
珍奶 <jary90705@gmail.com>
2021-12-31 20:46:28 +0800

    fix loading component

--- src/_metronic/partials/content/fallback-view/FallbackView.tsx
+++ src/_metronic/partials/content/fallback-view/FallbackView.tsx
@@ -2,9 +2,14 @@ import {toAbsoluteUrl} from '../../../helpers'
 
 export function FallbackView() {
   return (
-    <div className='splash-screen'>
-      <img src={toAbsoluteUrl('/media/logos/logo.png')} alt='Start logo' className='h-70px' />
-      <span>Loading ...</span>
+    <div className='splash-screen' style={{position: 'relative'}}>
+      <img
+        src={toAbsoluteUrl('/media/logos/logo.png')}
+        alt='Start logo'
+        className='h-70px'
+        style={{marginLeft: '0px'}}
+      />
+      <span>載入中 ...</span>
     </div>
   )
 }
