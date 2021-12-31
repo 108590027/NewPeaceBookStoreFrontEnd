6c855f9bb2789c7f7ddac07176745d910affae50
珍奶 <jary90705@gmail.com>
2021-12-31 21:58:29 +0800

    AdminReportsPage:: example remarks

--- src/app/modules/admin/components/AdminReportsPage.tsx
+++ src/app/modules/admin/components/AdminReportsPage.tsx
@@ -13,6 +13,7 @@ import getUnresolvedReportsAPI from '../../report/API/GetUnresolvedReportsAPI'
 import resolveReportAPI from '../../report/API/ResolveReportAPI'
 import {ReportState} from '../../report/redux/ReportRedux'
 
+// 麵包屑導航
 const BreadCrumbs: Array<PageLink> = [
   {
     title: '舉報系統管理',
@@ -23,31 +24,45 @@ const BreadCrumbs: Array<PageLink> = [
 ]
 
 const AdminReportsPage: FC = () => {
+  // 取得 Redux 的 AuthState
   const userState: IAuthState = useSelector((state: RootState) => state.auth)
+  // 取得 Redux 的 ReportState
   const reportState: ReportState = useSelector((state: RootState) => state.report)
+  // 儲存決斷的ID用
   const [resolveId, setResolveId] = useState(0)
+  // 儲存決斷的封禁天數用
   const [resolveTime, setResolveTime] = useState(0)
+  // 判斷初始化使用，避免因re-render而重複發送API打資料
   const [load, setLoad] = useState(false)
+  // 確定決斷
   const resolveReport = async () => {
+    // 打API，等待結果並取出
     const data = await resolveReportAPI(resolveId, resolveTime)
+    // 成功的回傳，理論上data就會有`id`這個屬性，藉此做判斷是否成功
     if ('id' in data) {
-      toast.success('回報成功')
+      toast.success('回報成功') // 右下角顯示提示訊息
     } else {
-      toast.error(`回報失敗${data.message}`)
+      toast.error(`回報失敗${data.message}`) // 右下角顯示錯誤訊息
     }
   }
+  // 打開決斷的彈窗
   const openResolveModal = (id: number) => {
+    // 先找出舉報的資料
     const report = reportState.reports.find((r) => r.id === id)
-    if (report) {
-      setResolveId(id)
-      setResolveTime(0)
-      new Modal('#resolveModal').show()
+    if (report?.status === 0) {
+      // 若舉報的資料存在，且尚未處理
+      setResolveId(id) // 設定目前選擇的舉報ID
+      setResolveTime(0) // 將彈窗中的封禁日期輸入欄歸0
+      new Modal('#resolveModal').show() // 打開彈窗
     }
   }
+  // 預初始化所有舉報中，檢舉人/被檢舉人的資訊 (僅打API取得目前尚未拿到的會員)
   const loadUsers = () => {
     reportState.reports.forEach((report) => {
+      // 從userState中撈出 檢舉人/被檢舉人的資訊
       const reporter = userState.users.find((u) => u.id === report.reporter)
       const victim = userState.users.find((u) => u.id === report.victim)
+      // 若沒撈到，則打API向後端請求資訊
       if (!reporter) {
         getUserAPI(report.reporter)
       }
@@ -56,15 +71,17 @@ const AdminReportsPage: FC = () => {
       }
     })
   }
+  // 依照UserID，從userState中取資料
   const getUser = (userId: number) => {
     return userState.users.find((u) => u.id === userId)
   }
+  // 進行初始化判斷
   if (!load) {
-    setLoad(true)
+    setLoad(true) // 初始化只會執行一次
     ;(async () => {
-      await getUnresolvedReportsAPI()
-      await getResolvedReportsAPI()
-      loadUsers()
+      await getUnresolvedReportsAPI() // 打未處理的舉報資料
+      await getResolvedReportsAPI() // 打已處理的舉報資料
+      loadUsers() // 抓取使用者資訊
     })()
   }
 
@@ -87,11 +104,15 @@ const AdminReportsPage: FC = () => {
                 </tr>
               </thead>
               <tbody>
+                {/* 陣列使用`map`方法，將每個元素轉換成指定的格式輸出 */}
                 {reportState.reports.map((report) => (
                   <tr key={report.id}>
+                    {/* 若是使用迴圈印HTML，則最外層的標籤須加上唯一屬性`key` 否則會跳錯誤 如上方 <tr key=id> */}
                     <td>{report.id}</td>
                     <td className='fw-bolder'>
+                      {/* Link是超連結，可以導向到to的連結去 */}
                       <Link to={`/admin/user/${report.victim}`}>
+                        {/* 使用||運算子：有撈到使用者資訊的話顯示名稱，沒有則直接顯示ID */}
                         {getUser(report.victim)?.name || `UserID:${report.victim}`}
                       </Link>
                     </td>
@@ -103,6 +124,7 @@ const AdminReportsPage: FC = () => {
                     <td className='fw-bolder'>{report.reason}</td>
                     <td className='fw-bolder'>{report.detail}</td>
                     <td>
+                      {/* 當舉報為未處理的狀態(status為0)，則可以進行決斷 => 顯示按鈕 */}
                       {report.status === 0 ? (
                         <>
                           <button
