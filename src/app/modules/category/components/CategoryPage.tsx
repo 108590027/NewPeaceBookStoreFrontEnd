93af4820bc0631bf0607dd57bca77d5e3458bf24
珍奶 <jary90705@gmail.com>
2022-01-01 17:10:44 +0800

    CategoryPage getCategoryItemsAPI

--- src/app/modules/category/components/CategoryPage.tsx
+++ src/app/modules/category/components/CategoryPage.tsx
@@ -1,10 +1,11 @@
-import React, {FC} from 'react'
+import React, {FC, useState} from 'react'
 import {useSelector} from 'react-redux'
 import {match} from 'react-router'
 import {RootState} from '../../../../setup'
 import {PageTitle} from '../../../../_metronic/layout/core'
 import {Card4} from '../../../../_metronic/partials/content/cards/Card4'
 import {ItemState} from '../../item/redux/ItemRedux'
+import getCategoryItemsAPI from '../API/GetCategoryItemsAPI'
 import {CategoryState} from '../redux/CategoryRedux'
 
 interface Props {
@@ -14,10 +15,21 @@ interface Props {
 const CategoryPage: FC<Props> = (props) => {
   const categoryState: CategoryState = useSelector((state: RootState) => state.category)
   const itemState: ItemState = useSelector((state: RootState) => state.item)
-  const category = categoryState.categories.find((c) => c.id === parseInt(props.match.params.id))
+  const [load, setLoad] = useState(false)
+  const [currentId, setCurrentId] = useState(0) // 紀錄目前的分類ID
+  if (parseInt(props.match.params.id) !== currentId) {
+    // 當route的分類ID變動時，必須進行更新
+    setLoad(false)
+    setCurrentId(parseInt(props.match.params.id))
+  }
+  const category = categoryState.categories.find((c) => c.id === currentId)
   const items = itemState.items.filter(
-    (item) => item.category.id === parseInt(props.match.params.id) && item.quantity > 0
+    (item) => item.category.id === currentId && item.quantity > 0
   )
+  if (!load && currentId !== 0) {
+    setLoad(true)
+    getCategoryItemsAPI(currentId)
+  }
 
   return (
     <>
