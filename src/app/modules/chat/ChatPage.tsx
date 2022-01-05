3be191991edca055d299ecad054a32f0c7546a3a
珍奶 <jary90705@gmail.com>
2022-01-05 21:48:00 +0800

    ChatList

--- src/app/modules/chat/ChatPage.tsx
+++ src/app/modules/chat/ChatPage.tsx
@@ -1,42 +1,17 @@
 import React from 'react'
 import {Redirect, Route, Switch} from 'react-router-dom'
 import {PageLink, PageTitle} from '../../../system/layout/core'
-import {Private} from './components/Private'
-import {Group} from './components/Group'
-import {Drawer} from './components/Drawer'
-
-const chatBreadCrumbs: Array<PageLink> = [
-  {
-    title: 'Chat',
-    path: '/chat/private-chat',
-    isSeparator: false,
-    isActive: false,
-  },
-  {
-    title: '',
-    path: '',
-    isSeparator: true,
-    isActive: false,
-  },
-]
+import {Chat} from './components/Chat'
+const chatBreadCrumbs: Array<PageLink> = []
 
 const ChatPage: React.FC = () => {
   return (
     <Switch>
-      <Route path='/chat/private-chat'>
-        <PageTitle breadcrumbs={chatBreadCrumbs}>Private chat</PageTitle>
-        <Private />
+      <Route path='/chat'>
+        <PageTitle breadcrumbs={chatBreadCrumbs}>聊天室</PageTitle>
+        <Chat />
       </Route>
-      <Route path='/chat/group-chat'>
-        <PageTitle breadcrumbs={chatBreadCrumbs}>Group chat</PageTitle>
-        <Group />
-      </Route>
-      <Route path='/chat/drawer-chat'>
-        <PageTitle breadcrumbs={chatBreadCrumbs}>Drawer chat</PageTitle>
-        <Drawer />
-      </Route>
-      <Redirect from='/chat' exact={true} to='/chat/private-chat' />
-      <Redirect to='/chat/private-chat' />
+      <Redirect to='/chat' />
     </Switch>
   )
 }
