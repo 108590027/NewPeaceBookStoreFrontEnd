eb8440e4b8a93abdee8ff7f68bc833471d2b3c12
yao <avex1103@gmail.com>
2022-01-06 11:27:45 +0800

    Shopping Cart Page can change quentity and delete cart item and show merchant name by item owner

--- src/app/modules/item/components/ShoppingCartPage.tsx
+++ src/app/modules/item/components/ShoppingCartPage.tsx
@@ -1,16 +1,20 @@
+import {Modal} from 'bootstrap'
 import React, {FC, useState} from 'react'
-import {shallowEqual, useSelector} from 'react-redux'
+import {useSelector} from 'react-redux'
+import {toast} from 'react-toastify'
+import {KTSVG} from '../../../../system/helpers'
 import {PageTitle} from '../../../../system/layout/core'
 import {RootState} from '../../../../setup'
 import {ItemState} from '../../item/redux/ItemRedux'
 import {CartState, actions} from '../redux/CartRedux'
+import {dispatch} from '../../../../setup/redux/Store'
 import getItemAPI from '../API/GetItemsAPI'
 
 const ShoppingCartPage: FC = () => {
   const CartState: CartState = useSelector((state: RootState) => state.cart)
   const itemState: ItemState = useSelector((state: RootState) => state.item)
-  const [load, setLoad] = useState(false)
-  const [currentId, setCurrentId] = useState(0)
+  const [updateItem, setUpdateItem] = useState(0)
+  const [updateQuantity, setUpdateQuantity] = useState(0)
 
   const getUserName = (itemId: number) => {
     let item = itemState.items.find((item) => item.id === itemId)
@@ -22,6 +26,15 @@ const ShoppingCartPage: FC = () => {
     return item
   }
 
+  /*const getItem = async (itemId: number) => {
+    const item = await getItemAPI(itemId)
+    if ('id' in item) {
+      return item
+    } else {
+      toast.warn('取得商品失敗')
+    }
+  }*/
+
   const calculatePrice = (price: number | undefined, quantity: number) => {
     if (!price) {
       price = 0
@@ -29,12 +42,24 @@ const ShoppingCartPage: FC = () => {
     return price * quantity
   }
 
-  const deleteItem = (id: number) => {
-    //actions.deleteCartItem(id)
+  const openUpdateModal = (id: number, quantity: number) => {
+    setUpdateItem(id)
+    setUpdateQuantity(quantity)
+    let item = itemState.items.find((item) => item.id === id)
+    if (item) {
+      new Modal('#updateModal').show()
+    }
   }
 
-  const modifyItem = (id: number) => {
-    //actions.updateCartItem(id,3)
+  const deleteItem = (id: number) => {
+    toast.success('已刪除商品')
+    dispatch(actions.deleteCartItem(id))
+  }
+
+  const modifyItem = () => {
+    dispatch(actions.updateCartItem(updateItem, updateQuantity))
+    document.getElementById('updateModalCancel')?.click()
+    toast.success(`已修改商品數量`)
   }
 
   return (
@@ -42,7 +67,7 @@ const ShoppingCartPage: FC = () => {
       <PageTitle breadcrumbs={[]}>{`我的購物車`}</PageTitle>
       <div className='col-12'>
         {/* 因為目前沒辦法加東西進購物車，所以先暫時直接指定某個item id */}
-        <h5>賣家名稱：{getUserName(CartState.Carts[0]?.itemId || 1) || '目前沒有任何商品！'}</h5>
+        <h5>賣家名稱：{getUserName(CartState.Carts[0]?.itemId) || '目前沒有任何商品！'}</h5>
         <div className='card card-xxl-stretch mb-5 mb-xxl-8'>
           <div className='table-responsive'>
             <table className='table table-hover table-rounded table-striped gy-4 gs-7 text-center align-middle'>
@@ -56,44 +81,16 @@ const ShoppingCartPage: FC = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>2020</td>
-                  <td>買</td>
-                  <td>100</td>
-                  <td>100</td>
-                  <td>
-                    <button className='btn btn-primary btn-sm mx-2' onClick={() => modifyItem(0)}>
-                      <i className='bi bi-pencil-square fs-5'></i>修改數量
-                    </button>
-                    <button className='btn btn-danger btn-sm mx-2' onClick={() => deleteItem(0)}>
-                      <i className='bi bi-trash-fill fs-5'></i>刪除
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>2021</td>
-                  <td>賣</td>
-                  <td>100</td>
-                  <td>100</td>
-                  <td>
-                    <button className='btn btn-primary btn-sm mx-2' onClick={() => modifyItem(1)}>
-                      <i className='bi bi-pencil-square fs-5'></i>修改數量
-                    </button>
-                    <button className='btn btn-danger btn-sm mx-2' onClick={() => deleteItem(1)}>
-                      <i className='bi bi-trash-fill fs-5'></i>刪除
-                    </button>
-                  </td>
-                </tr>
                 {CartState.Carts.map((item) => (
                   <tr>
-                    <td>{item.itemId}</td>
+                    <td>{getInfo(item.itemId)?.name}</td>
                     <td>{item.quantity}</td>
                     <td>{getInfo(item.itemId)?.price}</td>
                     <td>{calculatePrice(getInfo(item.itemId)?.price, item.quantity)}</td>
                     <td>
                       <button
                         className='btn btn-primary btn-sm mx-2'
-                        onClick={() => modifyItem(item.itemId)}
+                        onClick={() => openUpdateModal(item.itemId, item.quantity)}
                       >
                         <i className='bi bi-pencil-square fs-5'></i>修改數量
                       </button>
@@ -110,6 +107,56 @@ const ShoppingCartPage: FC = () => {
             </table>
           </div>
         </div>
+        {/* begin::修改數量的modal */}
+        <div className='modal fade' tabIndex={-1} id='updateModal'>
+          <div className='modal-dialog'>
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <h5 className='modal-title'>修改商品數量</h5>
+                <div
+                  className='btn btn-icon btn-sm btn-active-light-primary ms-2'
+                  data-bs-dismiss='modal'
+                  aria-label='Close'
+                >
+                  <KTSVG
+                    path='/media/icons/duotune/arrows/arr061.svg'
+                    className='svg-icon svg-icon-2x'
+                  />
+                </div>
+              </div>
+              <div className='modal-body'>
+                <div className='row'>
+                  <div className='col-12 mt-4'>
+                    <label className='form-check-label' htmlFor={`updateQuentity`}>
+                      商品數量
+                    </label>
+                    <input
+                      id='updateQuentity'
+                      value={updateQuantity}
+                      type='number'
+                      className='form-control mt-1'
+                      onChange={(e) => setUpdateQuantity(parseInt(e.target.value))}
+                    ></input>
+                  </div>
+                </div>
+              </div>
+              <div className='modal-footer'>
+                <button
+                  id='updateModalCancel'
+                  type='button'
+                  className='btn btn-light'
+                  data-bs-dismiss='modal'
+                >
+                  取消
+                </button>
+                <button type='button' className='btn btn-primary' onClick={(e) => modifyItem()}>
+                  修改
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end::修改數量的modal */}
         <div className='d-flex justify-content-end mt-8'>
           <button className='btn btn-primary'>
             <span className='indicator-label '>下訂</span>
