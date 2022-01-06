19c1d35f1d9dfd4f43b07b19d766f77bb31c6c24
jerry12122 <a0975844268@gmail.com>
2022-01-06 21:24:45 +0800

    itemPage

--- src/app/modules/item/components/ItemPage.tsx
+++ src/app/modules/item/components/ItemPage.tsx
@@ -6,20 +6,23 @@ import {PageLink, PageTitle} from '../../../../system/layout/core'
 import {ItemState} from '../../item/redux/ItemRedux'
 import getItemAPI from '../API/GetItemsAPI'
 import {Swiper, SwiperSlide} from 'swiper/react'
-import 'swiper/css'
-import {KTSVG} from '../../../../system/helpers'
 import {Link} from 'react-router-dom'
 import SwiperCore, {Navigation} from 'swiper'
 import * as CartRedux from '../redux/CartRedux'
 import {toast} from 'react-toastify'
 import {dispatch} from '../../../../setup/redux/Store'
+import 'swiper/css/bundle'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import '../css/swiper.css'
 
 // install Swiper modules
 SwiperCore.use([Navigation])
+
 const BreadCrumbs: Array<PageLink> = [
   {
     title: '商品頁面',
-    path: '/item/:id',
+    path: `/`,
     isSeparator: false,
     isActive: false,
   },
@@ -33,7 +36,8 @@ const ItemPage: FC<Props> = (props) => {
   const [currentId, setCurrentId] = useState(0)
   const itemState: ItemState = useSelector((state: RootState) => state.item)
   const item = itemState.items.find((item) => item.id === currentId)
-  const [itemCount, setItemCount] = useState(0)
+  const [itemCount, setItemCount] = useState(1)
+
   if (parseInt(props.match.params.id) !== currentId) {
     // 當route的分類ID變動時，必須進行更新
     setLoad(false)
@@ -43,9 +47,14 @@ const ItemPage: FC<Props> = (props) => {
     setLoad(true)
     getItemAPI(currentId)
   }
-  const addToCart = async () => {
+
+  const addToCart = async (quantity: number) => {
     if (itemCount === 0) {
       toast.warn('請選擇商品數量')
+      return
+    }
+    if (itemCount > quantity) {
+      toast.warn('商品數量不足')
       return
     }
 
@@ -54,20 +63,25 @@ const ItemPage: FC<Props> = (props) => {
   }
   return (
     <>
-      <PageTitle breadcrumbs={[]}>{`${item?.name}`}</PageTitle>
-      <div className='card mb-5 mb-xl-10'>
-        <div className='card-header cursor-pointer'>
-          <div className='card-title m-0'>
-            <h3 className='fw-bolder m-0'>{item?.name}</h3>
-          </div>
-        </div>
-      </div>
+      <PageTitle breadcrumbs={BreadCrumbs}>{`${item?.name}`}</PageTitle>
+
       <div className='row gy-5 g-xl-8'>
         <div className='col-xxl-4 '>
           <div className='card  mb-5 mb-xl-10 text-center mw-100'>
-            {item?.images.map((image, i) => (
-              <img src={image.photo} alt='' />
-            ))}
+            {item?.images[0] ? (
+              <Swiper navigation={true}>
+                {item?.images.map((image, i) => (
+                  <SwiperSlide>
+                    <img
+                      src={image ? image.photo : '/media/icons/duotune/ecommerce/ecm005.svg'}
+                      alt=''
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            ) : (
+              <img src='/media/icons/duotune/ecommerce/ecm005.svg' alt='' />
+            )}
           </div>
         </div>
 
@@ -79,17 +93,30 @@ const ItemPage: FC<Props> = (props) => {
           </div>
           <div className='card-body border-top p-9'>
             <h1 className='text-danger mb-3'>${item?.price}</h1>
+            <br></br>
+            <Link to={`/chat#${item?.owner.id}`} className='col-auto '>
+              <h5 className='text-primary'>聯繫賣家</h5>
+            </Link>
           </div>
-          <div className='card-body border-top p-9 row'>
-            <h3 className='col-lg-4'>數量</h3>
+
+          <div className='card-body border-top p-9'>
+            <div className='row'>
+              <h3 className='col-auto'>數量</h3>
+              <h5 className='col-auto text-muted'>剩餘數量：{item?.quantity}個</h5>
+            </div>
+
             <input
               type='number'
-              className='form-control form-control-solid col-lg-8'
+              className='form-control form-control-solid'
               value={itemCount}
               onChange={(e) => setItemCount(parseInt(e.target.value))}
             />
           </div>
-          <button className='btn btn-lg btn-primary w-100 mb-5' onClick={() => addToCart()}>
+
+          <button
+            className='btn btn-lg btn-primary w-100 mb-5'
+            onClick={() => addToCart(item?.quantity as number)}
+          >
             加入購物車
           </button>
         </div>
