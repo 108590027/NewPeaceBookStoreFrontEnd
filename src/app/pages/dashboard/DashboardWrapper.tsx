ac56cf90a54ed8502c8b366c9d2ec551a53997b3
jerry12122 <a0975844268@gmail.com>
2022-01-08 15:41:46 +0800

    Dashboard update

--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -1,96 +1,88 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, {FC} from 'react'
+import React, {FC, useState} from 'react'
+import {useSelector} from 'react-redux'
+import {toast} from 'react-toastify'
+import {RootState} from '../../../setup'
 import {PageTitle} from '../../../system/layout/core'
-import {
-  MixedWidget2,
-  MixedWidget10,
-  MixedWidget11,
-  ListsWidget2,
-  ListsWidget3,
-  ListsWidget4,
-  ListsWidget5,
-  ListsWidget6,
-  TablesWidget5,
-  TablesWidget10,
-  MixedWidget8,
-} from '../../../system/partials/widgets'
-
-const DashboardPage: FC = () => (
-  <>
-    {/* begin::Row */}
-    <div className='row gy-5 g-xl-8'>
-      <div className='col-xxl-4'>
-        <MixedWidget2
-          className='card-xl-stretch mb-xl-8'
-          chartColor='danger'
-          chartHeight='200px'
-          strokeColor='#cb1e46'
-        />
-      </div>
-      <div className='col-xxl-4'>
-        <ListsWidget5 className='card-xxl-stretch' />
-      </div>
-      <div className='col-xxl-4'>
-        <MixedWidget10
-          className='card-xxl-stretch-50 mb-5 mb-xl-8'
-          chartColor='primary'
-          chartHeight='150px'
-        />
-        <MixedWidget11
-          className='card-xxl-stretch-50 mb-5 mb-xl-8'
-          chartColor='primary'
-          chartHeight='175px'
-        />
-      </div>
-    </div>
-    {/* end::Row */}
-
-    {/* begin::Row */}
-    <div className='row gy-5 gx-xl-8'>
-      <div className='col-xxl-4'>
-        <ListsWidget3 className='card-xxl-stretch mb-xl-3' />
-      </div>
-      <div className='col-xl-8'>
-        <TablesWidget10 className='card-xxl-stretch mb-5 mb-xl-8' />
-      </div>
-    </div>
-    {/* end::Row */}
-
-    {/* begin::Row */}
-    <div className='row gy-5 g-xl-8'>
-      <div className='col-xl-4'>
-        <ListsWidget2 className='card-xl-stretch mb-xl-8' />
-      </div>
-      <div className='col-xl-4'>
-        <ListsWidget6 className='card-xl-stretch mb-xl-8' />
-      </div>
-      <div className='col-xl-4'>
-        <ListsWidget4 className='card-xl-stretch mb-5 mb-xl-8' items={5} />
-        {/* partials/widgets/lists/_widget-4', 'class' => 'card-xl-stretch mb-5 mb-xl-8', 'items' => '5' */}
-      </div>
-    </div>
-    {/* end::Row */}
-
-    <div className='row g-5 gx-xxl-8'>
-      <div className='col-xxl-4'>
-        <MixedWidget8
-          className='card-xxl-stretch mb-xl-3'
-          chartColor='success'
-          chartHeight='150px'
-        />
-      </div>
-      <div className='col-xxl-8'>
-        <TablesWidget5 className='card-xxl-stretch mb-5 mb-xxl-8' />
-      </div>
-    </div>
-  </>
-)
+import {Card4} from '../../../system/partials/content/cards/Card4'
+import getHotItemsAPI from '../../modules/item/API/GetHotItemsAPI'
+import getNewItemsAPI from '../../modules/item/API/GetNewItemsAPI'
+import {ItemModel} from '../../modules/item/redux/ItemModel'
+import {ItemState} from '../../modules/item/redux/ItemRedux'
 
 const DashboardWrapper: FC = () => {
+  const [load, setLoad] = useState(false)
+  const [hotItems, setHotItems] = useState<ItemModel[]>([])
+  const [newItems, setNewItems] = useState<ItemModel[]>([])
+  const getHotItems = async () => {
+    const result = await getHotItemsAPI()
+    if ('message' in result) {
+      toast.error('取得熱門商品失敗')
+    } else {
+      setHotItems([...result])
+    }
+  }
+  const getNewItems = async () => {
+    const result = await getNewItemsAPI()
+    if ('message' in result) {
+      toast.error('取得最新商品失敗')
+    } else {
+      setNewItems([...result])
+    }
+  }
+  if (!load) {
+    setLoad(true)
+    getHotItems()
+    getNewItems()
+  }
+
   return (
     <>
       <PageTitle breadcrumbs={[]}>{`首頁`}</PageTitle>
-      <DashboardPage />
+      <div className='card mb-5 mb-xl-10'>
+        <div className='card-header cursor-pointer'>
+          <div className='card-title m-0'>
+            <h3 className='fw-bolder m-0'>推薦商品</h3>
+          </div>
+        </div>
+      </div>
+      <div className='row g-6 g-xl-9 mb-6 mb-xl-9'>
+        {hotItems.map((item) => (
+          <div className='col-12 col-sm-6 col-lg-4 col-xl-3' key={item.id}>
+            <Card4
+              icon={
+                item.images[0] ? item.images[0].photo : '/media/icons/duotune/ecommerce/ecm005.svg'
+              }
+              title={item.name}
+              description={`$${item.price}`}
+              isBase64Image={item.images[0]?.photo !== undefined}
+              id={item.id}
+            />
+          </div>
+        ))}
+      </div>
+      <div className='card mb-5 mb-xl-10'>
+        <div className='card-header cursor-pointer'>
+          <div className='card-title m-0'>
+            <h3 className='fw-bolder m-0'>最新商品</h3>
+          </div>
+        </div>
+      </div>
+      <div className='row g-6 g-xl-9 mb-6 mb-xl-9'>
+        {newItems.map((item) => (
+          <div className='col-12 col-sm-6 col-lg-4 col-xl-3' key={item.id}>
+            <Card4
+              icon={
+                item.images[0] ? item.images[0].photo : '/media/icons/duotune/ecommerce/ecm005.svg'
+              }
+              title={item.name}
+              description={`$${item.price}`}
+              isBase64Image={item.images[0]?.photo !== undefined}
+              id={item.id}
+            />
+          </div>
+        ))}
+      </div>
     </>
   )
 }
