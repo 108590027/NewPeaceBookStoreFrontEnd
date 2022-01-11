1df3ccefadb9e443536bf8a50064c3e398925a0e
Eggy <louisl0517l@gmail.com>
2022-01-11 11:25:09 +0800

    評價

--- src/app/modules/accounts/components/settings/Analytic.tsx
+++ src/app/modules/accounts/components/settings/Analytic.tsx
@@ -18,6 +18,7 @@ export function Analytic() {
 
   const dayBuyPrice: number[] = new Array(31).fill(0)
   const daySellPrice: number[] = new Array(31).fill(0)
+  const starNumber: number[] = new Array(5).fill(0)
   const dateTemp: number[] = []
   for (let k = 0; k < 31; k++) {
     dateTemp[k] = k + 1
@@ -30,6 +31,12 @@ export function Analytic() {
   data?.data.sellOrders.forEach((element) => {
     const date = new Date(element.created_at.replace('T', ' ')).getDate()
     daySellPrice[date - 1] += element.total_price
+  })
+  data?.data.rates.forEach((element, i) => {
+    if (i === 0) {
+      return
+    }
+    starNumber[i] = element
   })
   const chartOptions = {
     chart: {
@@ -55,6 +62,33 @@ export function Analytic() {
       },
     },
   }
+
+  const chartOptions1 = {
+    chart: {
+      height: 350,
+      type: 'bar' as 'bar',
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'smooth' as 'smooth',
+    },
+    title: {
+      text: `${'評價數量'}`,
+      align: 'left' as 'left',
+    },
+    xaxis: {
+      categories: ['1星', '2星', '3星', '4星', '5星'],
+    },
+  }
+
   const lines = [
     {
       name: '買入',
@@ -62,20 +96,26 @@ export function Analytic() {
     },
     {
       name: '賣出',
-      data: [11, 32, 45, 32, 34, 52, 41],
+      data: [] as number[],
+    },
+  ]
+  const bar = [
+    {
+      data: [] as number[],
     },
   ]
   if (data) {
     lines[0].data = dayBuyPrice
     lines[1].data = daySellPrice
+    bar[0].data = starNumber
   }
-
+  console.log(data)
   return (
     <div className='card mb-5 mb-xl-10'>
       <div className='card-body border-top p-9'>
         <div className='row mb-6'>
           <ReactApexChart options={chartOptions} series={lines} type='area' height={600} />
-
+          <ReactApexChart options={chartOptions1} series={bar} type='bar' height={600} />
           <div className='col-lg-8'>
             <div className='row'>
               <div className='col-lg-6 fv-row'></div>
