c808d0c87aab9816b88f0be1629371d68090976a
珍奶 <jary90705@gmail.com>
2022-01-13 15:45:51 +0800

    fix LayoutTitle

--- src/system/layout/MasterLayout.tsx
+++ src/system/layout/MasterLayout.tsx
@@ -5,9 +5,12 @@ import {HeaderWrapper} from './components/header/HeaderWrapper'
 import {ScrollTop} from './components/ScrollTop'
 import {Content} from './components/Content'
 import {MasterInit} from './MasterInit'
-import {PageDataProvider} from './core'
+import {LayoutTitle} from './LayoutTitle'
+import {PageDataProvider, useLayout} from './core'
+import clsx from 'clsx'
 
 const MasterLayout: React.FC = ({children}) => {
+  const {classes} = useLayout()
   return (
     <PageDataProvider>
       <div className='page d-flex flex-row flex-column-fluid'>
@@ -16,6 +19,32 @@ const MasterLayout: React.FC = ({children}) => {
           <HeaderWrapper />
 
           <div id='kt_content' className='content d-flex flex-column flex-column-fluid'>
+            <div className='toolbar' id='kt_toolbar'>
+              {/* begin::Container */}
+              <div
+                id='kt_toolbar_container'
+                className={clsx(classes.toolbarContainer.join(' '), 'd-flex flex-stack')}
+              >
+                <LayoutTitle />
+
+                {/* begin::Actions */}
+                <div className='d-flex align-items-center py-1'>
+                  {/* begin::Wrapper */}
+                  <div className='me-4'>
+                    {/* begin::Menu */}
+
+                    {/* end::Menu */}
+                  </div>
+                  {/* end::Wrapper */}
+
+                  {/* begin::Button */}
+
+                  {/* end::Button */}
+                </div>
+                {/* end::Actions */}
+              </div>
+              {/* end::Container */}
+            </div>
             <div className='post d-flex flex-column-fluid' id='kt_post'>
               <Content>{children}</Content>
             </div>
